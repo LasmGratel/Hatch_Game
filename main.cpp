038a8878efd54e1d9ab8fc001adbd717c9c215d2
BuildTools <unconfigured@null.spigotmc.org>
2015-05-18 20:55:51 +0800

    add recond

--- main.cpp
+++ main.cpp
@@ -15,31 +15,48 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <iostream>
-using namespace std;
-typedef unsigned int uint;
+#include "headers.h"
 uint GET = 32767;
 uint MAX = 2147483647;
-uint n, total = 0, i, player_win = 0, computer_win = 0;
+uint n, total = 0, i, player_win = 0, computer_win = 0, t = 0;
 bool b = false;
+string recond_str[100000];
+void recond(string x)
+{
+	recond_str[t++] = x;
+}
+void recond_func()
+{
+	system("cls");
+	for (uint i = 0;i < t;i++) cout << recond_str[i] << endl;
+	system("pause");
+	system("cls");
+	return;
+}
 void play()
 {
-	total = 0;n = 0;
+	recond(" ");
+	recond("开始游戏...");
+	system("CLS");
+	total = 0;n = 0;b = false;
 	while (cin >> n)
 	{
 		if (n == 0)
 		{
 			cout << "你必须拿走至少1根火柴!";
+			recond("玩家拿走了少于1根火柴");
 			continue;
 		}
 		else if (n > GET)
 		{
 			cout << "你不能拿走多于" << GET << "根火柴！";
+			recond("玩家拿走了多于" + to_string(GET) + "根火柴");
 			continue;
 		}
 		else if (n >= MAX - total)
 		{
 			cout << "你不能拿走多于当前剩余的" << MAX - total << "根火柴！";
+			recond("玩家拿走了多于当前剩余的" + to_string(MAX - total) + "根火柴");
 			continue;
 		}
 		total += n;
@@ -48,12 +65,14 @@ void play()
 		if (total == MAX - 1 || total == MAX)
 		{
 			cout << "您输了！" << endl;
+			recond("玩家输了");
 			++computer_win;
 			break;
 		}
-		else if (total >= MAX - GET + 1)
+		else if (total >= MAX - 1)
 		{
 			cout << "计算机输了！" << endl;
+			recond("计算机输了");
 			++player_win;
 			break;
 		}
@@ -64,6 +83,7 @@ void play()
 				if (total + i < MAX - 1)
 				{
 					cout << "计算机拿走" << i << "根火柴" << endl;
+					recond("计算机拿走了" + to_string(i) + "根火柴");
 					total += i;
 					break;
 				}
@@ -74,15 +94,18 @@ void play()
 			}
 			if (b)
 			{
-				cout << "计算机别无他法，准备拿走3根火柴。" << endl;
-				total += 3;
+				cout << "计算机别无他法，准备拿走1根火柴。" << endl;
+				recond("计算机拿走了1根火柴");
+				total += 1;
 			}
 		}
 	}
-	return;
+	recond_func();
 }
 int main()
 {
+main_func:
+	system("cls");
 	cout << "输出版权信息:\n" << "Hatch Game\n" << \
 		"Copyright(C) 2015 Lasm_Gratel.\n" << \
 		'\n' << \
@@ -104,8 +127,10 @@ int main()
 	{
 		cout << "请输入每次最多拿多少根火柴:";
 		cin >> GET;
+		recond("玩家自定义每次最多拿" + to_string(GET) + "根火柴");
 		cout << "请输入总共有多少根火柴:";
 		cin >> MAX;
+		recond("玩家自定义每次总共有" + to_string(GET) + "根火柴");
 	}
 	cout << "当前计算机赢了" << computer_win << "局，玩家赢了" << player_win << "局。";
 	cout << "游戏规则:" << endl;
@@ -119,6 +144,6 @@ int main()
 	cout << "想重来一遍吗？输入1重来,输入0退出:";
 	cin >> n;
 	if (n)
-		play();
+		goto main_func;
 	return 0;
 }
